99562b9cb3ec2c88bab9925a16a241a675e5b263
NguyenGiangDev <nguyengiangdevw@gmail.com>
2025-04-21 15:00:22 +0700

    deploy IaC

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,7 +25,7 @@ variable "ssh_pub_key_path" {
 
 variable "vm_names" {
   type    = list(string)
-  default = ["master-node", "worker-node-1", "worker-node-2"]
+  default = ["master", "slave"]
 }
 
 variable "machine_type" {
