905b987caaf776a1c36d964887930d5c2db83fc3
NguyenGiangDev <nguyengiangdevw@gmail.com>
2025-04-30 16:31:21 +0700

    Update generate_inventory.sh

--- ansible/generate_inventory.sh
+++ ansible/generate_inventory.sh
@@ -30,15 +30,10 @@ common_args="ansible_ssh_common_args='-o StrictHostKeyChecking=no -o UserKnownHo
 
 # Tạo phần [master]
 echo "[master]" > "$inventory_file"
-echo "$output" | jq -r --arg args "$common_args" '. as $ips | to_entries[] |
-  select(.key == "master") |
-  "\(.key) ansible_host=\(.value) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/my_gcp_key \($args)"' >> "$inventory_file"
-
-# Tạo phần [workers]
-echo -e "\n[slave]" >> "$inventory_file"
-echo "$output" | jq -r --arg args "$common_args" '. as $ips | to_entries[] |
-  select(.key | test("^slave")) |
-  "\(.key) ansible_host=\(.value) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/my_gcp_key \($args)"' >> "$inventory_file"
+echo "$output" | jq -r '. as $ips | to_entries[] | select(.key == "master-node") | "\(.key) ansible_host=\(.value) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/my_gcp_key"' >> "$inventory_file"
+# Tạo phần [worker]
+echo -e "\n[workers]" >> "$inventory_file"
+echo "$output" | jq -r '. as $ips | to_entries[] | select(.key | test("^worker-node")) | "\(.key) ansible_host=\(.value) ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/my_gcp_key"' >> "$inventory_file"
 
 echo -e "\n✅ Đã tạo xong inventory.ini tại thư mục ansible:"
 cat "$inventory_file"
