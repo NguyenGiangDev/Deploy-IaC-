5dcf7306b387f22f844bc8be74bb0955f97a30a7
NguyenGiangDev <nguyengiangdevw@gmail.com>
2025-04-30 22:54:16 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,7 +25,7 @@ variable "ssh_pub_key_path" {
 
 variable "vm_names" {
   type    = list(string)
-  default = ["worker-node-1", "worker-node-2", "worker-node-3", "worker-node-4"]
+  default = ["worker-node-1", "worker-node-2", "worker-node-3"]
 }
 
 variable "machine_type" {
